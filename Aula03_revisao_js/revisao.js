aa32911e2861966a969dd217b3eec1538652fb84
test1 <pedrosantanawork8@gmail.com>
2025-02-28 21:19:16 -0300

    Primeira parte de revisão js

---
+++ Aula03_revisao_js/revisao.js
@@ -0,0 +1,53 @@
+console.log("teste")
+// primeiro comentario
+
+    /* segundo comentario
+    para relembrar
+    */
+
+    // variaveis 
+    const nome = "Pedro"
+    let idade = 20
+    const maiorDeIdade = true
+
+    console.log(nome)
+    console.log(idade)
+    console.log("E maior de idade? ", maiorDeIdade)
+    
+    idade = 19
+    console.log(idade)
+    
+    // operadores
+    const soma = 2 + 2
+    const subtracao = 2-2
+    const multiplicacao = 2*2
+    const divisao = 2/2
+
+    console.log(soma)
+    console.log(subtracao)
+    console.log(multiplicacao)
+    console.log(divisao)
+
+    const resto = 3 % 3
+  let numero  = 4
+   let incremento = numero ++
+    let decremento = numero --
+    
+    console.log("incremento ->",incremento)
+    console.log("decremento ->",decremento)
+    console.log("resto ->", resto)
+
+    let a = 3
+    a %= 3
+    console.log(a)
+
+    // opredores de comparação
+const igual = 10 =="10"
+    console.log(igual)
+    const estritamenteigual = 10 ==="10"
+        console.log(estritamenteigual)
+const maior = 3 > 1
+const menor =  4 < 10
+const maiorouigual = 10>= 10
+const menorouigual = 10<= 10
+
